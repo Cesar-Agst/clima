437dbfb2c6acf9f8af0bfad1657502976ae3fe3c
Cesar Augusto <cesaar883@gmail.com>
2021-03-15 15:06:24 -0300

    erro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from 'react-bootstrap/Navbar'
 import { Nav } from 'react-bootstrap'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function App(){
 
@@ -9,11 +10,18 @@ function App(){
     <>{/*React Fragment*/}
         <Navbar bg="primary">
           <Navbar.Brand href="#inicio">FateClima</Navbar.Brand>
-          <Nav classname="mr-auto">
-          <Nav.link href="#inicio">Início</Nav.link>
-          <Nav.link href="#contao">Contato</Nav.link>
+          <Nav className="mr-auto">
+             <Nav.Link href="#inicio">Início</Nav.Link>
+            <Nav.Link href="#contao">Contato</Nav.Link>
           </Nav>
        </Navbar>
+       <Jumbotron>
+         <h1>FateClima</h1>
+         <p>
+           Consulte o clima de qualquer cidade do mundo. <br></br>
+           App desenvolvido em ReactJS e integrado com API OpenWeatherMap.
+         </p>
+       </Jumbotron>
     </>
   )
 }
