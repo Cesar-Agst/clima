a5ba90f2c1b234990cb715dcdbb9f600f4e2f737
Cesar Augusto <cesaar883@gmail.com>
2021-03-15 14:55:10 -0300

    hj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from 'react-bootstrap/Navbar'
+import { Nav } from 'react-bootstrap'
 
 function App(){
 
   return(
     <>{/*React Fragment*/}
-
+        <Navbar bg="primary">
+          <Navbar.Brand href="#inicio">FateClima</Navbar.Brand>
+          <Nav classname="mr-auto">
+          <Nav.link href="#inicio">Início</Nav.link>
+          <Nav.link href="#contao">Contato</Nav.link>
+          </Nav>
+       </Navbar>
     </>
   )
 }
